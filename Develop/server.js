1c89fde83f58a1d461ea6fffc390db553ed25b56
macosta2015 <macosta2015@my.fit.edu>
2022-06-14 00:48:38 -0400

    commit

--- Develop/server.js
+++ Develop/server.js
@@ -9,60 +9,57 @@ const router = express.Router(); //We are not using the library to the left. It 
 //Port # that we are going to be using
 const port = process.env.PORT || 9090; 
 
+
+
 //Initialize express 
 const app = express();
-
-// CLIENTE ----------------------------------------------------------------
-// 
-// 
-//
-// CLIENT
-
-
-//we are defining a variable databaseObject and giving it the value of the content within our json file, we then parse it so that it returns a javascript object. 
-let databaseObject = JSON.parse(fs.readFileSync("Develop/db/db.json", "utf-8", (err)=> {
-  if(err) cosnole.log('Error');
-}));
-
 
 //Middleware for parsing JSON. 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+// CLIENTE ----------------------------------------------------------------
+// 
+// 
+//
+// 
+
+
+//we are defining a variable databaseObject and giving it the value of the content within our json file, we then parse it so that it returns a javascript object. 
+let databaseObject = JSON.parse(fs.readFileSync("Develop/db/db.json", "utf-8", (err)=> {
+  if(err){
+    cosnole.log('Error');
+  }
+}));
+
+
+
 //This allow us to use the assets folder that contains all the HTML/CSS/JS
 app.use(express.static(path.join(__dirname, 'Develop/public')));
 
 
-
-
-//APP .GET REQUESTS START 
-//Gets the main index.html file and returns it to the user when they open the page/app.
-app.get("/", (req, res) => {
-    console.log("here")
-    res.sendFile(path.join(__dirname + "/Develop/public/index.html"))
-})
-
 //Gets the notes.html file and returns it to the user when the url contains mainpage/notes 
 app.get("/notes", (req, res)  => {
-    res.sendFile(path.join(__dirname + "/Develop/public/notes.html" ))
+  console.info(`${req.method} request received `);
+  console.info(`We are in the main HTML, http://localhost:9090/`);
+  res.sendFile(path.join(__dirname + "/Develop/public/notes.html" ))
 });
 
-//Returns the notes within the json file
+
+//NOTE that "/api/notes" is coming from index.js
 app.get("/api/notes", (req, res) => {
+  console.info(`${req.method} request received `);
   return res.json(databaseObject);
 });
 
-//APP.GET REQUESTS ENDS 
-
-
-//APP.POST REQUESTS STARTS 
-//adds notes into the json file within the server
 app.post("/api/notes", (req, res) => {
-  let newNote = {title: req.body.title, text: req.body.text} //our newNote will contain an array with 2 parameters
-  newNote.id = databaseObject.length.toString(); //converts it to a string
+  console.info(`${res.method} request received `);
+  let noteInformation = {title: req.body.title, text: req.body.text} 
 
-  databaseObject.push(newNote); //pushes the newNote into the databaseObject 
+  noteInformation.id = databaseObject.length.toString(); //converts it to a string
+
+  databaseObject.push(noteInformation); //pushes the noteInformation into the databaseObject 
 
   fs.writeFile("./Develop/db/db.json", JSON.stringify(databaseObject), //writes in the file databaseObject as a string within said array
   (err)=>{
@@ -71,35 +68,16 @@ app.post("/api/notes", (req, res) => {
    res.json(databaseObject)
 });
 
-//APP.POST REQUESTS END HERE (ONLY 1)
-
-
-//APP.DELETE REQUEST STARTS 
-app.delete("/api/notes/:id",  (req, res) => {
-  let idSelected = JSON.parse(req.params.id);//the selected id will depend on the note that the user clicked on
-
-  databaseObject = databaseObject.filter((e) => { //filters out the databaseObject and returns it without the id that was selected 
-    return e.id != idSelected;
-  });
-
-  databaseObject.forEach((val, index) => { //assigns each string within the array a new id
-    val.id = index.toString();
-  });
-
-  //writes on the json file serverside the new values without the selected id that is deleted. and then turns the databaseObject into a string so that it displays as one. and we can get the new values without the deleted ones.
-  fs.writeFile("./Develop/db/db.json", JSON.stringify(databaseObject), (err) => {
-    if(err) 
-    throw err;
-  });
-  res.end();
+//IF we only do http://localhost:9090/ we are going to get at the main page, which is index.html
+app.get("/", (req, res) => {
+  console.info(`${res.method} request received `);
+  res.sendFile(path.join("/Develop/public/index.html"))
+  // res.sendFile(path.join("index.html"))
 })
 
-//APP.DELETE REQUEST ENDS 
-
-
-//allows us to see the server with all our individual requests get, post, del.
+//THIS ALWAYS NEEDS TO GO AT LAST
 app.listen(port, () =>
-console.log(`Listening at http://localhost:${port} `)
+console.log(`Port http://localhost:${port} is listening`)
 );
 
 
